e79b1fa82dea051344002df311ecd4c1705d7032
Luis <luismanuelhn@gmail.com>
2021-11-01 21:44:46 -0600

    primer commit para repositorio

---
+++ src/main/java/Helpers/Persona.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Helpers;
+
+/**
+ *
+ * @author Familia
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String sexo;
+    private int edad;
+    private String identificacion;
+    
+    
+    
+}
